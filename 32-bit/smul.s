0c022275970c7e949c82b9d974d57acec6153c90
maksnowak <59841779+maksnowak@users.noreply.github.com>
2023-06-08 13:27:09 +0200

    Update comments

--- 32-bit/smul.s
+++ 32-bit/smul.s
@@ -7,7 +7,7 @@
 
     section .text
     global smul
-; prolog
+; Prolog
 smul:
     push ebp
     mov ebp, esp
@@ -38,32 +38,35 @@ second_loop:
     jnz second_loop
     dec edx     ; Zmniejsz długość łańcucha o 1 - ostatni znak to null
 multiply:
+    ; Przygotowania do wykonania mnożenia pisemnego
     mov esi, ecx    ; Zapisz długość pierwszego łańcucha w rejestrze
-    push ecx    ; Zapisz długość pierwszego łańcucha na stosie
+    push ecx    ; Zapisz długość pierwszego łańcucha na stosie - zabrakło rejestrów
     mov ebx, [ebp+8]    ; Wskaźnik na łańcuch wynikowy
-    add ebx, esi    ; Przesuń wskaźnik na koniec łańcucha wynikowego
+    add ebx, esi    ; Przesuń wskaźnik wynikowy o długość pierwszego łańcucha
     inc ebx     ; Przesuń wskaźnik na pierwszy znak poza łańcuchem wynikowym
     add esi, [ebp+12]  ; Przesuń wskaźnik na koniec pierwszego łańcucha
 multiplicand:
-    add ebx, edx    ; Przesuń wskaźnik na koniec łańcucha wynikowego
-    dec ebx     ; Przesuń wskaźnik na ostatni znak łańcucha wynikowego
+    ; Mnożenie mnożnej przez kolejne cyfry mnożnika
+    add ebx, edx    ; Przesuń wskaźnik wynikowy o długość drugiego łańcucha
+    dec ebx     ; Przesuń wskaźnik o jeden w lewo
     dec esi    ; Przesuń wskaźnik na następną cyfę mnożnej
     mov cl, [esi]    ; Wczytaj cyfrę mnożnej
     ; Przejdź do ostatniej cyfry mnożnika
     mov edi, edx
     add edi, [ebp+16]
 multiplier:
-    dec ebx     ; Przesuń wskaźnik na następną cyfrę wyniku
-    dec edi     ; Przesuń wskaźnik na następną cyfrę mnożnika
+    ; Mnożenie pisemne - jeden bajt to jedna cyfra
+    dec ebx
+    dec edi
     mov al, [edi]    ; Wczytaj cyfrę mnożnika
     mul cl      ; Pomnóż cyfrę mnożnika przez cyfrę mnożnej
-    add al, [ebx]    ; Dodaj cyfrę wyniku
+    add al, [ebx]    ; Dodaj wartość przeniesienia z poprzedniego mnożenia
     aam     ; Skoryguj wynik
-    add [ebx-1], ah     ; Dodaj cyfrę dziesiątek do poprzedniej cyfry wyniku
-    mov [ebx], al   ; Zapisz cyfrę jedności w wyniku
-    cmp edi, [ebp+16]   ; Sprawdź czy to ostatnia cyfra mnożnika
+    add [ebx-1], ah     ; Dodaj przeniesienie
+    mov [ebx], al   ; Zapisz cyfrę jedności
+    cmp edi, [ebp+16]   ; Sprawdź czy cały mnożnik został przemnożony
     jne multiplier
-    cmp esi, [ebp+12]   ; Sprawdź czy to ostatnia cyfra mnożnej
+    cmp esi, [ebp+12]   ; Sprawdź czy cała mnożna została przemnożona
     jne multiplicand
 remove_zeros:
     mov eax, [ebp+8]    ; Wskaźnik na łańcuch wynikowy
@@ -71,26 +74,31 @@ remove_zeros:
     test cl, cl     ; Sprawdź czy to null
     jnz done     ; Jeśli nie, to zakończ
     inc eax     ; Przesuń wskaźnik na następną cyfrę wyniku
-    dec edx     ; Przesuń wskaźnik na następną cyfrę mnożnej
+    dec edx     ; Zmniejsz długość drugiego łańcucha (i wyniku) o 1
+
+    ; Jeśli na początku łańcucha są dwa zera, to wynikiem mnożenia jest zero
     mov cl, [eax]   ; Wczytaj następną cyfrę wyniku
     test cl, cl     ; Sprawdź czy to null
     jnz done      ; Jeśli tak, to zakończ
-    ; Jeśli na początku łańcucha są dwa zera, to wynikiem mnożenia jest zero
     dec eax     ; Przesuń wskaźnik na poprzednią cyfrę wyniku
     add byte [eax], '0' ; Zamień null na zero
-    pop ecx
+    pop ecx ; Opróżnij stos
     jmp fin
 done:
+    ; Przygotuj do zmiany wyniku na postać ASCII
     mov ebx, eax
-    pop ecx
+    pop ecx ; Opróżnij stos
+    ; Przesuń wskaźnik o długość łańcucha wynikowego
     add ebx, ecx
     add ebx, edx
 convert_ascii:
+    ; Konwertuj wynik na postać ASCII, dopóki nie dojdziesz do początku łańcucha
     dec ebx
     add byte [ebx], '0'
     cmp ebx, eax
     jg convert_ascii
 fin:
+    ; Epilog
     pop edi
     pop esi
     pop ebx
