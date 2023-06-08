138e765306b2233e22c753574cf0ea85e7765a69
maksnowak <59841779+maksnowak@users.noreply.github.com>
2023-06-08 12:51:24 +0200

    smul done

--- 32-bit/smul.s
+++ 32-bit/smul.s
@@ -14,60 +14,83 @@ smul:
     push ebx
     push esi
     push edi
-    mov esi, [ebp+12] ; s1
-    mov edi, [ebp+16] ; s2
-    mov bl, 0   ; carry
-    mov bh, 10 ; potrzebne do sprawdzenia przeniesienia
-    mov eax, 0 ; zerowanie rejestru
-    mov ecx, 0 ; zerowanie rejestru
-iterate_s1:
-    mov al, [esi]
-    test al, al
-    jz iterate_s2
-    inc esi
-    jmp iterate_s1
-iterate_s2:
-    mov al, [edi]
-    test al, al
-    jz multiply
-    inc edi
-    jmp iterate_s2
+    xor eax, eax    ; Wyczyść zawartość eax
+convert_first:
+    xor ecx, ecx    ; Wyczyść rejestr przechowujący długość łańcucha
+    mov esi, [ebp+12]   ; Wskaźnik na pierwszy łańcuch
+first_loop:
+    mov al, [esi+ecx]   ; Wczytaj n-ty znak z łańcucha (n = ecx)
+    sub al, '0'     ; Zamień znak na odpowiadającą mu liczbę
+    xchg al, [esi+ecx]  ; Zamień liczbę i odpowiadający jej znak
+    inc ecx     ; Zwiększ długość łańcucha
+    test al, al     ; Sprawdź czy null
+    jnz first_loop
+    dec ecx     ; Zmniejsz długość łańcucha o 1 - ostatni znak to null
+convert_second:
+    xor edx, edx    ; Wyczyść rejestr przechowujący długość łańcucha
+    mov edi, [ebp+16]   ; Wskaźnik na drugi łańcuch
+second_loop:
+    mov al, [edi+edx]   ; Wczytaj n-ty znak z łańcucha (n = edx)
+    sub al, '0'     ; Zamień znak na odpowiadającą mu liczbę
+    xchg al, [edi+edx]  ; Zamień liczbę i odpowiadający jej znak
+    inc edx     ; Zwiększ długość łańcucha
+    test al, al     ; Sprawdź czy null
+    jnz second_loop
+    dec edx     ; Zmniejsz długość łańcucha o 1 - ostatni znak to null
 multiply:
-    dec esi
-    dec edi
-multiply_digit:
-    mov al, [esi]
-    mov ah, [edi]
-    cmp al, 0
-    jz next_digit
-    dec esi
-    sub al, '0'
-    sub ah, '0'
-    mul ah
-    aam
-    ; dodaj cyfrę jedności wyniku
-    add [ecx], al
-    ; dodaj cyfrę dziesiątek wyniku
-    add [ecx+1], ah
-    add [ecx+1], bl ; dodaj przeniesienie
-    ; sprawdź, czy jest przeniesienie
-    cmp [ecx+1], bh
-    jge carry
-    ; jeśli nie ma przeniesienia, to zwiększ wskaźnik na wynik
-    inc ecx
-    jmp multiply_digit
-carry:
-    ; jeśli jest przeniesienie, zachowaj jego wartość do rejestru cl
-    mov bl, 1
-    ; oraz usuń je z wyniku
-    sub [ecx+1], bh
-    ; zwiększ wskaźnik na wynik
-    inc ecx
-    jmp multiply_digit
-next_digit:
-; epilog
+    mov esi, ecx    ; Zapisz długość pierwszego łańcucha w rejestrze
+    push ecx    ; Zapisz długość pierwszego łańcucha na stosie
+    mov ebx, [ebp+8]    ; Wskaźnik na łańcuch wynikowy
+    add ebx, esi    ; Przesuń wskaźnik na koniec łańcucha wynikowego
+    inc ebx     ; Przesuń wskaźnik na pierwszy znak poza łańcuchem wynikowym
+    add esi, [ebp+12]  ; Przesuń wskaźnik na koniec pierwszego łańcucha
+multiplicand:
+    add ebx, edx    ; Przesuń wskaźnik na koniec łańcucha wynikowego
+    dec ebx     ; Przesuń wskaźnik na ostatni znak łańcucha wynikowego
+    dec esi    ; Przesuń wskaźnik na następną cyfę mnożnej
+    mov cl, [esi]    ; Wczytaj cyfrę mnożnej
+    ; Przejdź do ostatniej cyfry mnożnika
+    mov edi, edx
+    add edi, [ebp+16]
+multiplier:
+    dec ebx     ; Przesuń wskaźnik na następną cyfrę wyniku
+    dec edi     ; Przesuń wskaźnik na następną cyfrę mnożnika
+    mov al, [edi]    ; Wczytaj cyfrę mnożnika
+    mul cl      ; Pomnóż cyfrę mnożnika przez cyfrę mnożnej
+    add al, [ebx]    ; Dodaj cyfrę wyniku
+    aam     ; Skoryguj wynik
+    add [ebx-1], ah     ; Dodaj cyfrę dziesiątek do poprzedniej cyfry wyniku
+    mov [ebx], al   ; Zapisz cyfrę jedności w wyniku
+    cmp edi, [ebp+16]   ; Sprawdź czy to ostatnia cyfra mnożnika
+    jne multiplier
+    cmp esi, [ebp+12]   ; Sprawdź czy to ostatnia cyfra mnożnej
+    jne multiplicand
+remove_zeros:
+    mov eax, [ebp+8]    ; Wskaźnik na łańcuch wynikowy
+    mov cl, [eax]   ; Wczytaj pierwszą cyfrę wyniku
+    test cl, cl     ; Sprawdź czy to null
+    jnz done     ; Jeśli nie, to zakończ
+    inc eax     ; Przesuń wskaźnik na następną cyfrę wyniku
+    dec edx     ; Przesuń wskaźnik na następną cyfrę mnożnej
+    mov cl, [eax]   ; Wczytaj następną cyfrę wyniku
+    test cl, cl     ; Sprawdź czy to null
+    jnz done      ; Jeśli tak, to zakończ
+    ; Jeśli na początku łańcucha są dwa zera, to wynikiem mnożenia jest zero
+    dec eax     ; Przesuń wskaźnik na poprzednią cyfrę wyniku
+    add byte [eax], '0' ; Zamień null na zero
+    pop ecx
+    jmp fin
+done:
+    mov ebx, eax
+    pop ecx
+    add ebx, ecx
+    add ebx, edx
+convert_ascii:
+    dec ebx
+    add byte [ebx], '0'
+    cmp ebx, eax
+    jg convert_ascii
 fin:
-    mov eax, [ebp+8]
     pop edi
     pop esi
     pop ebx
